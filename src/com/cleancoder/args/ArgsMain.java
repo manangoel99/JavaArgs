ca7b05e8a4fad0ee93f58f567716aa865aa1506b
Manan Goel <manan.goel@research.iiit.ac.in>
2020-01-22 17:58:21 +0530

    Implemented argument parsing for double, map and String Array

--- src/com/cleancoder/args/ArgsMain.java
+++ src/com/cleancoder/args/ArgsMain.java
@@ -1,17 +1,20 @@
 package com.cleancoder.args;
 
+import java.util.*;
+
 public class ArgsMain {
   public static void main(String[] args) {
     try {
-      Args      arg       = new Args("f,s*,n#,a##,p[*]", args);
-      boolean   logging   = arg.getBoolean('f');
-      int       port      = arg.getInt('n');
-      String    directory = arg.getString('s');
-      double    ip        = arg.getDouble('a');
-      String[]  map       = arg.getStringArray('p');
+      Args                arg             = new Args("f,s*,n#,a##,p[*],d&", args);
+      boolean             logging         = arg.getBoolean('f');
+      int                 port            = arg.getInt('n');
+      String              directory       = arg.getString('s');
+      double              ip              = arg.getDouble('a');
+      String[]            string_array    = arg.getStringArray('p');
+      Map<String, String> map             = arg.getMap('d');
       
 
-      executeApplication(logging, port, directory, ip, map);
+      executeApplication(logging, port, directory, ip, string_array, map);
 
     } 
     catch (ArgsException e) {
@@ -19,7 +22,7 @@ public class ArgsMain {
     }
   }
 
-  private static void executeApplication(boolean logging, int port, String directory, double ip, String[] map) {
+  private static void executeApplication(boolean logging, int port, String directory, double ip, String[] string_array, Map<String, String> map) {
     System.out.printf(
       "Boolean is %s, Integer is %d, String is %s, Double is %f\n",
       logging, 
@@ -28,8 +31,9 @@ public class ArgsMain {
       ip
     );
     System.out.println("String array elements are : ");
-    for (int i = 0; i < map.length; i++) {
-        System.out.printf("%s\n", map[i]);
+    for (int i = 0; i < string_array.length; i++) {
+        System.out.printf("%s\n", string_array[i]);
     }
+    System.out.println(map.toString());
   }
 }
