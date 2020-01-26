ae75bfd76011a79942f8d6f03efa5fcc55212cec
Manan Goel <manan.goel@research.iiit.ac.in>
2020-01-26 18:18:36 +0530

    Implemented main method for ArgsExceptionText

--- test/com/cleancoder/args/ArgsExceptionTest.java
+++ test/com/cleancoder/args/ArgsExceptionTest.java
@@ -4,64 +4,78 @@ import junit.framework.TestCase;
 
 import static com.cleancoder.args.ArgsException.ErrorCode.*;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 public class ArgsExceptionTest extends TestCase {
-public void testUnexpectedMessage() throws Exception {
-        ArgsException e = new ArgsException(UNEXPECTED_ARGUMENT, 'x', null);
+        public static void main(String[] args) {
+                Result result = JUnitCore.runClasses(ArgsExceptionTest.class);
 
-        assertEquals("Argument -x unexpected.", e.errorMessage());
-}
+                for (Failure failure : result.getFailures()) {
+                        System.out.println(failure.toString());
+                }
 
-public void testMissingStringMessage() throws Exception {
-        ArgsException e = new ArgsException(MISSING_STRING, 'x', null);
+                System.out.println(result.wasSuccessful());
+        }
 
-        assertEquals("Could not find string parameter for -x.", e.errorMessage());
-}
+        public void testUnexpectedMessage() throws Exception {
+                ArgsException e = new ArgsException(UNEXPECTED_ARGUMENT, 'x', null);
 
-public void testInvalidIntegerMessage() throws Exception {
-        ArgsException e = new ArgsException(INVALID_INTEGER, 'x', "Forty two");
+                assertEquals("Argument -x unexpected.", e.errorMessage());
+        }
 
-        assertEquals("Argument -x expects an integer but was 'Forty two'.", e.errorMessage());
-}
+        public void testMissingStringMessage() throws Exception {
+                ArgsException e = new ArgsException(MISSING_STRING, 'x', null);
 
-public void testMissingIntegerMessage() throws Exception {
-        ArgsException e = new ArgsException(MISSING_INTEGER, 'x', null);
+                assertEquals("Could not find string parameter for -x.", e.errorMessage());
+        }
 
-        assertEquals("Could not find integer parameter for -x.", e.errorMessage());
-}
+        public void testInvalidIntegerMessage() throws Exception {
+                ArgsException e = new ArgsException(INVALID_INTEGER, 'x', "Forty two");
 
-public void testInvalidDoubleMessage() throws Exception {
-        ArgsException e = new ArgsException(INVALID_DOUBLE, 'x', "Forty two");
+                assertEquals("Argument -x expects an integer but was 'Forty two'.", e.errorMessage());
+        }
 
-        assertEquals("Argument -x expects a double but was 'Forty two'.", e.errorMessage());
-}
+        public void testMissingIntegerMessage() throws Exception {
+                ArgsException e = new ArgsException(MISSING_INTEGER, 'x', null);
 
-public void testMissingDoubleMessage() throws Exception {
-        ArgsException e = new ArgsException(MISSING_DOUBLE, 'x', null);
+                assertEquals("Could not find integer parameter for -x.", e.errorMessage());
+        }
 
-        assertEquals("Could not find double parameter for -x.", e.errorMessage());
-}
+        public void testInvalidDoubleMessage() throws Exception {
+                ArgsException e = new ArgsException(INVALID_DOUBLE, 'x', "Forty two");
 
-public void testMissingMapMessage() throws Exception {
-        ArgsException e = new ArgsException(MISSING_MAP, 'x', null);
+                assertEquals("Argument -x expects a double but was 'Forty two'.", e.errorMessage());
+        }
 
-        assertEquals("Could not find map string for -x.", e.errorMessage());
-}
+        public void testMissingDoubleMessage() throws Exception {
+                ArgsException e = new ArgsException(MISSING_DOUBLE, 'x', null);
 
-public void testMalformedMapMessage() throws Exception {
-        ArgsException e = new ArgsException(MALFORMED_MAP, 'x', null);
+                assertEquals("Could not find double parameter for -x.", e.errorMessage());
+        }
 
-        assertEquals("Map string for -x is not of form k1:v1,k2:v2...", e.errorMessage());
-}
+        public void testMissingMapMessage() throws Exception {
+                ArgsException e = new ArgsException(MISSING_MAP, 'x', null);
 
-public void testInvalidArgumentName() throws Exception {
-        ArgsException e = new ArgsException(INVALID_ARGUMENT_NAME, '#', null);
+                assertEquals("Could not find map string for -x.", e.errorMessage());
+        }
 
-        assertEquals("'#' is not a valid argument name.", e.errorMessage());
-}
+        public void testMalformedMapMessage() throws Exception {
+                ArgsException e = new ArgsException(MALFORMED_MAP, 'x', null);
 
-public void testInvalidFormat() throws Exception {
-        ArgsException e = new ArgsException(INVALID_ARGUMENT_FORMAT, 'x', "$");
+                assertEquals("Map string for -x is not of form k1:v1,k2:v2...", e.errorMessage());
+        }
 
-        assertEquals("'$' is not a valid argument format.", e.errorMessage());
-}
+        public void testInvalidArgumentName() throws Exception {
+                ArgsException e = new ArgsException(INVALID_ARGUMENT_NAME, '#', null);
+
+                assertEquals("'#' is not a valid argument name.", e.errorMessage());
+        }
+
+        public void testInvalidFormat() throws Exception {
+                ArgsException e = new ArgsException(INVALID_ARGUMENT_FORMAT, 'x', "$");
+
+                assertEquals("'$' is not a valid argument format.", e.errorMessage());
+        }
 }
